70f787f90aae74adad044d8ebeffa412cd2affe6
Arik Dasen <arik.dasen@six-group.com>
2017-12-10 16:57:26 +0100

    init

---
+++ src/main/java/ch/carve/os/hello/ApplicationBase.java
@@ -0,0 +1,9 @@
+package ch.carve.os.hello;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/")
+public class ApplicationBase extends Application {
+
+}
---
+++ src/main/java/ch/carve/os/hello/HelloResource.java
@@ -0,0 +1,14 @@
+package ch.carve.os.hello;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
+@Path("/")
+public class HelloResource {
+
+    @Path("hello")
+    @GET
+    public String getHello() {
+        return "Hello Openshift";
+    }
+}
